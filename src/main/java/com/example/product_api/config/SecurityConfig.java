edbb1ac2341d919a577a28f4f0453772e4ca7752
Nitheshk42 <nitheshk42@gmail.com>
2025-11-11 15:16:09 -0500

    updates some changes in securityconfig

--- src/main/java/com/example/product_api/config/SecurityConfig.java
+++ src/main/java/com/example/product_api/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,4 +23,14 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}password")  // {noop} means plain text password
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 }
+
